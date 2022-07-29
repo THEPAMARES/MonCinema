981101cd2daf0ee2f5ff9e833a65d5ace3b5b9b9
Pamela TAVARES <pamela.tavares@groupe-cyllene.com>
2022-07-29 11:46:04 +0200

    changement index URL

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 // session_start();
-define("URL",str_replace("index.php","",(isset($_SERVER['HTTPS']) ? "https" : "http")."://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
+if (!defined("URL")) define("URL",str_replace("index.php","",(isset($_SERVER['HTTPS']) ? "https" : "http")."://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
+
 
 require "controllers/MovieController.controller.php";
 
